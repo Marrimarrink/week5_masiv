21c50f72ea9b5b48b3597bde0c3d90775d225bda
Marrimarrink <hihiop@mail.ru>
2024-10-24 23:10:14 +0300

    Initial commit

---
+++ src/main/java/_main/Main.java
@@ -0,0 +1,53 @@
+package _main;
+
+import java.util.stream.IntStream;
+
+import static util.Calc.*;
+
+public class Main {
+    public static void main(String[] arg) {
+        int[] arr = new int[]{5, -3, 2, 1, 7, 11, 12, -4, 1, 10};
+
+        // Длина массива = 10
+        // System.out.println(arr.length);
+        //Вывести все элементы массива
+       /* for (int i = 0; i < 10; i++) {
+            System.out.println(arr[i]);
+        }*/
+
+        //  1. количество четных чисел в массиве = 4// Пример массива
+        int evenCount = countEvenNum(arr);
+        System.out.println("Кол-во четных чисел в массиве: " + evenCount);
+
+        //2. количество нечетных чисел в массиве = 6
+        int oddCount = countOddNum(arr); // Вызов метода для подсчета нечетных чисел
+        System.out.println("Кол-во нечетных чисел в массиве: " + oddCount);
+
+        //3. количество простых чисел в массиве = 4
+        int primeCount = countPrimeNum(arr);
+        System.out.println("Кол-во простых чисел в массиве: " + primeCount);
+
+        // 4. сумму всех чисел в массиве = 42
+        int sumAllNum = IntStream.of(arr).sum();
+        System.out.println("Сумма всех чисел в массиве: " + sumAllNum);
+
+        //  5. разность между суммой чисел имеющих четный индекс и суммой чисел имеющих нечетный индекс в массиве =12
+
+        int diffAllSumm = countDiffSum(arr);
+        System.out.println("Разность между суммой чисел имеющих четный индекс и суммой чисел имеющих нечетный индекс в массиве: " + diffAllSumm);
+
+
+        //  6. количество 0 (сколько раз встречается число "0") в массиве = 0
+        int nullCount = countNullNum(arr);
+        System.out.println("Кол-во 0 (сколько раз встречается число \"0\") в массивее: " + nullCount);
+
+//System.out.println(Arrays.binarySearch(arr,0));
+
+     /*   int key = Arrays.binarySearch(arr, 7); // ищем key - число 5 в отсортированном массиве.
+        //метод binarySearch выдаст индекс элемента остортированного массива, в котором "спрятано" искомое число
+        System.out.println(key);*/
+
+
+    }
+}
+
---
+++ src/main/java/util/Calc.java
@@ -0,0 +1,77 @@
+package util;
+
+import java.util.stream.IntStream;
+
+public class Calc {
+
+    public static int countEvenNum(int[] array) {
+        int count = 0;
+        for (int num : array) {
+            if (num % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countOddNum(int[] array) {
+        int count = 0; // Переменная для хранения количества нечетных чисел
+        for (int num : array) { // Перебор каждого числа в массиве
+            if (num % 2 != 0) { // Проверка, является ли число нечетным
+                count++; // Увеличиваем счетчик, если число нечетное
+            }
+        }
+        return count; // Возвращаем общее количество нечетных чисел
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int countPrimeNum(int[] array) {
+        int count = 0;
+        for (int num : array) {
+            if (isPrime(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean isNull(int num) {
+        return num == 0;
+    }
+
+    public static int countNullNum(int[] array) {
+        int count = 0;
+        for (int num : array) {
+            if (isNull(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static int countDiffSum(int[] array){
+
+        int sumEvenNum = 0;
+        int sumOddNum = 0;
+        for (int num = 0; num < array.length; num++)
+            if (num % 2 == 0) {
+                sumEvenNum += array[num]; // Добавляем элемент с четным индексом к sumEvenNum
+            } else {
+                sumOddNum += array[num];
+            }
+        return  sumEvenNum - sumOddNum;
+}
+
+
+
+}
