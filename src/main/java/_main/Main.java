a0942351d6b908ad2d60d1ea0801eae05c3a592e
Marrimarrink <hihiop@mail.ru>
2024-10-28 21:55:26 +0300

    добавлено решение сортировка по возрастанию

--- src/main/java/_main/Main.java
+++ src/main/java/_main/Main.java
@@ -7,25 +7,32 @@ import static util.Calc.*;
 
 public class Main {
     public static void main(String[] arg) {
-        int[] arr = new int[10];
+        int[] arr1 = new int[10];
         Random random = new Random();
 
         // Заполнить  массив случайными числами от -10 до 10
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(21) - 10;
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = random.nextInt(21) - 10;
         }
-
 
         System.out.print("Сгенерированный массив: ");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
+        printArray(arr1);
+
+        int[] arr2 = new int[]{2, 3, 1, 6};
 
 
 
+        System.out.println("                                                ");
+        System.out.println("--------------------------------------------------");
 
-        //int[] arr = new int[]{5, -3, 2, 1, 7, 11, 12, -4, 1, 10};
+        sortArray(arr2);
+        System.out.print("Сортировка массива по возрастанию: ");
+        printArray(arr2);
+
+        System.out.print("Сортировка массива в обратном порядке: ");
+        printRevertArray(arr2);
+
+        // int[] arr = new int[]{5, -3, 2, 1, 7, 11, 12, -4, 1, 10};
 
         // Длина массива = 10
         // System.out.println(arr.length);
@@ -35,33 +42,34 @@ public class Main {
         }*/
 
         //  1. количество четных чисел в массиве = 4// Пример массива
-        int evenCount = countEvenNum(arr);
+        int evenCount = countEvenNum(arr1);
         System.out.println("Кол-во четных чисел в массиве: " + evenCount);
 
         //2. количество нечетных чисел в массиве = 6
-        int oddCount = countOddNum(arr); // Вызов метода для подсчета нечетных чисел
+        int oddCount = countOddNum(arr1); // Вызов метода для подсчета нечетных чисел
         System.out.println("Кол-во нечетных чисел в массиве: " + oddCount);
 
         //3. количество простых чисел в массиве = 4
-        int primeCount = countPrimeNum(arr);
+        int primeCount = countPrimeNum(arr1);
         System.out.println("Кол-во простых чисел в массиве: " + primeCount);
 
         // 4. сумму всех чисел в массиве = 42
-       // int sumAllNum = IntStream.of(arr).sum();
-        int sumAllNum = sumAllNum(arr);
+        // int sumAllNum = IntStream.of(arr).sum();
+        int sumAllNum = sumAllNum(arr1);
         System.out.println("Сумма всех чисел в массиве: " + sumAllNum);
 
         //  5. разность между суммой чисел имеющих четный индекс и суммой чисел имеющих нечетный индекс в массиве =12
 
-        int diffAllSumm = countDiffSum(arr);
+        int diffAllSumm = countDiffSum(arr1);
         System.out.println("Разность между суммой чисел имеющих четный индекс и суммой чисел имеющих нечетный индекс в массиве: " + diffAllSumm);
 
 
         //  6. количество 0 (сколько раз встречается число "0") в массиве = 0
-        int nullCount = countNullNum(arr);
+        int nullCount = countNullNum(arr1);
         System.out.println("Кол-во 0 (сколько раз встречается число \"0\") в массивее: " + nullCount);
 
-//System.out.println(Arrays.binarySearch(arr,0));
+
+        //System.out.println(Arrays.binarySearch(arr,0));
 
      /*   int key = Arrays.binarySearch(arr, 7); // ищем key - число 5 в отсортированном массиве.
         //метод binarySearch выдаст индекс элемента остортированного массива, в котором "спрятано" искомое число
--- src/main/java/util/Calc.java
+++ src/main/java/util/Calc.java
@@ -60,7 +60,8 @@ public class Calc {
 
         return count;
     }
-    public static int countDiffSum(int[] array){
+
+    public static int countDiffSum(int[] array) {
 
         int sumEvenNum = 0;
         int sumOddNum = 0;
@@ -70,19 +71,46 @@ public class Calc {
             } else {
                 sumOddNum += array[num];
             }
-        return  sumEvenNum - sumOddNum;
-}
-
-
-
-public static int sumAllNum(int[] array){
-        int sum = 0;
-    for (int num : array) {
-        sum += num;
+        return sumEvenNum - sumOddNum;
     }
-    return sum;
-}
 
+    public static int sumAllNum(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static void sortArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printRevertArray(int[] array) {
+        int index = array.length - 1;
+        while (index >= 0) {
+            System.out.print(array[index] + " ");
+            index = index - 1;
+
+        }
+
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println(); // Переход на новую строку
+    }
 
 
 }
